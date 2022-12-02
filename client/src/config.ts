ef690ff5a26a91293e5f8d89caf9366d3986a069
Princenuel <emmanuel.chukwuemeka0416@gmail.com>
2022-12-02 14:48:35 +0100

    a redo of my serverless app to satisfy a reviewer

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,11 +1,11 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '680jdnvb52'
+const apiId = '5vxcpwxaf4'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: 'dev-0a81p3w2rl240x5z.us.auth0.com',// Auth0 domain
-  clientId: 'tM2XBURSYEKrSR0xoS8By0Y8VBrN1506',// Auth0 client id
+  domain: 'dev-jskguilpmsj2yvaw.us.auth0.com',// Auth0 domain
+  clientId: 'UdqEcUEDoz23qIVOBE3kBV0EeMYVqhIs',// Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
